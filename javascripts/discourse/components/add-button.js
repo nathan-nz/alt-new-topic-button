27efb7f78d40f8c88ae3688c10b2abce9725e8d1
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2023-12-13 12:12:28 +1300

    Update add-button.js

--- javascripts/discourse/components/add-button.js
+++ javascripts/discourse/components/add-button.js
@@ -1,5 +1,11 @@
-// add-button.js
 import Component from "@glimmer/component";
+import { action } from "@ember/object";
 
 export default class AddButtonComponent extends Component {
+  newTopicUrl = "https://ehealthforum.nz/w/ws-new-topic";
+
+  @action
+  goToNewTopic() {
+    window.location.href = this.newTopicUrl;
+  }
 }
