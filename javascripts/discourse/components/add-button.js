039a813a74bd212d416471657d4e66a4f996b5dc
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2023-12-13 13:58:23 +1300

    Update add-button.js

--- javascripts/discourse/components/add-button.js
+++ javascripts/discourse/components/add-button.js
@@ -1,33 +1,12 @@
 // add-button.js
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 
 export default class AddButtonComponent extends Component {
-  @service router;
-
-  get showAddButton() {
-    let alternativeLinks = JSON.parse(Discourse.SiteSettings.alternative_links);
-    let currentCategory = this.router.currentRoute.params.category;
-
-    // Check if the current category is in the list of specified categories
-    return alternativeLinks.some(link => link.category === currentCategory);
-  }
-
- get buttonData() {
-   let alternativeLinks = JSON.parse(Discourse.SiteSettings.alternative_links);
-   let currentCategory = this.router.currentRoute.params.category;
-
-   let buttonData = alternativeLinks.find(link => link.category === currentCategory);
-
-   // Log the output of the getter
-   console.log(buttonData);
- 
-   return buttonData;
- }
+  newTopicUrl = "https://ehealthforum.nz/w/ws-new-topic";
 
   @action
   goToNewTopic() {
-    window.location.href = this.buttonData.url;
+    window.location.href = this.newTopicUrl;
   }
 }
