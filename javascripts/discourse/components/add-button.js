c9eedf25e8c83e13fb652486a22ae908a9e33087
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2023-12-13 13:14:54 +1300

    Update add-button.js

--- javascripts/discourse/components/add-button.js
+++ javascripts/discourse/components/add-button.js
@@ -1,19 +1,20 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class AddButtonComponent extends Component {
-  @service settings;
+  @service router;
 
   get showAddButton() {
-    let alternativeLinks = JSON.parse(this.settings.alternative_links);
-    let currentCategory = this.args.category;
+    let alternativeLinks = JSON.parse(Discourse.SiteSettings.alternative_links);
+    let currentCategory = this.router.currentRoute.params.category;
 
     return alternativeLinks.some(link => link.category === currentCategory);
   }
 
   get buttonData() {
-    let alternativeLinks = JSON.parse(this.settings.alternative_links);
-    let currentCategory = this.args.category;
+    let alternativeLinks = JSON.parse(Discourse.SiteSettings.alternative_links);
+    let currentCategory = this.router.currentRoute.params.category;
 
     return alternativeLinks.find(link => link.category === currentCategory);
   }
